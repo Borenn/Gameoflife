d4c17b962226eec6a6daa4bda4e1958940967116
Borenn <erikbohren@gmail.com>
2017-03-02 15:48:12 +0100

    Update gameoflife.java

--- src/gameoflife/gameoflife.java
+++ src/gameoflife/gameoflife.java
@@ -15,7 +15,7 @@ public class gameoflife {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code applicationi logic here
     }
     
 }
